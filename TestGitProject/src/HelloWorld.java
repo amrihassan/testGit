16cd47139e6ec61f8cfdc6eb475a2a4818e3ff89
amrihassan <amrihassan@gmail.com>
2015-03-20 10:02:45 +0100

    Update HelloWorld.java

--- TestGitProject/src/HelloWorld.java
+++ TestGitProject/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello!!");
-		System.out.println("quoi de neuf");
+		System.out.println("quoi de pour merge neuf");
 		System.out.println("test pull");
 	}
 
